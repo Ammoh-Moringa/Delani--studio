4c60bbab4924e5bf01a14fa19746cbcfcbdd43a8
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-04-17 18:44:11 +0300

    Add a function for toggle

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $("#design").click(function(){
+    $("#designimg").toggle();
+    $("#designdesc").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#dev").click(function(){
+    $("#designimg").toggle();
+    $("#designdesc").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("#design").click(function(){
+    $("#designimg").toggle();
+    $("#designdesc").toggle();
+  });
+});
